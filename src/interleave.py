1e414e18831b720f7160a2cc5a63f1028782f9b0
anarchie <116115028+anarchie347@users.noreply.github.com>
2025-04-23 23:27:13 +0100

    added help screen

--- src/interleave.py
+++ src/interleave.py
@@ -1,6 +1,44 @@
 import pypdf
 import sys
 
+if len(sys.argv) < 2 or sys.argv[1] == '--help' or sys.argv[1] == '-h':
+    # Help menu
+    print("")
+    print("PDF Interleaver")
+    print("")
+    print("---------------")
+    print("")
+    print("Takes two PDFs and interleaves the pages")
+    print("Intended to combine 2 single-sided scans from a scanner with an auto document feed that doesnt support double sided scans")
+    print("")
+    print("---------------")
+    print("")
+    print("Usage:")
+    print("python interleave.py first.pdf second.pdf output.pdf [-r]")
+    print("")
+    print("  -r : reverse the second pdf. Usually needed if the pdfs are from an auto document feed")
+    print("")
+    print("---------------")
+    print("")
+    print("Requires:")
+    print("python, pypdf library")
+    print("")
+    print("---------------")
+    print("")
+    print("Print this page using -h, --help or providing no arguments")
+    print("")
+    print("---------------")
+    print("")
+    print("Written and mainted by anarchie347: https://github.com/anarchie347")
+    print("Project: https://github.com/anarchie347/pdf-interleaver/")
+    print("Issues: https://github.com/anarchie347/pdf-interleaver/issues")
+    print("Contribute: https://github.com/anarchie347/pdf-interleaver/pulls")
+    print("License: https://github.com/anarchie347/pdf-interleaver/blob/main/LICENSE")
+    print("")
+    exit()
+
+# Actual program
+
 firstpath = sys.argv[1]
 secpath = sys.argv[2]
 savepath = sys.argv[3]
